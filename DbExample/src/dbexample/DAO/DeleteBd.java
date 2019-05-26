c3b7504961b1fae0513d4cc92913d08ef8396111
GuiLakers <guifun08080808@gmail.com>
2019-05-26 23:51:22 +0100

    init

---
+++ DbExample/src/dbexample/DAO/DeleteBd.java
@@ -0,0 +1,32 @@
+
+package dbexample.DAO;
+
+import Entities.Register;
+import static dbexample.DAO.UpdateBd.conn;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+
+public class DeleteBd {
+     static Connection conn;
+    static PreparedStatement ps;
+    
+    public static boolean register(Register rg){
+        try {
+            
+            String DELETE_QUERY = "DELETE from register where Id=?";
+            conn = ConexaoBd.getConexaoMySQL();
+            ps = conn.prepareStatement(DELETE_QUERY);
+            ps.setInt(1, rg.getId());
+            ps.executeUpdate();
+            
+            ConexaoBd.FecharConexao();
+            return true;
+            
+        } catch (SQLException e) {
+            return false;
+        }
+    }
+    
+}

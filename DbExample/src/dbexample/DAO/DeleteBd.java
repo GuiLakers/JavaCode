35fe3d12a035056054bba90aae753a94a5f888f3
GuiLakers <guifun08080808@gmail.com>
2019-06-07 22:36:40 +0100

    init delete class

--- DbExample/src/dbexample/DAO/DeleteBd.java
+++ DbExample/src/dbexample/DAO/DeleteBd.java
@@ -2,7 +2,6 @@
 package dbexample.DAO;
 
 import Entities.Register;
-import static dbexample.DAO.UpdateBd.conn;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
